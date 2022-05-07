98ff4c4760ccf59be5d0376a0fb4f093623b2096
Olifan <olifananton@gmail.com>
2022-05-07 12:10:47 +0300

    Validate same city name

--- weather-app-react/src/weather/AddItem.js
+++ weather-app-react/src/weather/AddItem.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types"
 import WeatherList from "./WeatherList";
 import WeatherItem from "./WeatherItem";
 
+const cities = []
+
 
 function useInputValue(defaultValue = '') {
   const [value, setValue] = useState(defaultValue)
@@ -21,17 +23,27 @@ function AddItem({onCreate}) {
   const input = useInputValue('')
   const [message, setMessage] = useState('')
 
+  function addCity(){
+    cities.push(input.value().toLowerCase());
+    console.log(cities);
+  }
+
 
   async function submitHandler(event){
     event.preventDefault()
 
     if(!input.value() || input.value() === ''){
       setMessage('Please search for a valid city 😩')
-    }else if(input.value() === WeatherItem.key){
+    }else if(cities.includes(input.value().toLowerCase())){
       setMessage(`You already know the weather for ${input.value()}
       ...otherwise be more specific by providing the country code as well 😉`)
+
+      input.clear()
     }
     else {
+
+      addCity()
+
       let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value()}&appid=dc4a159c14374d585f7cdb4ed8afbf60&units=metric`)
       let data = await res.json()
 
